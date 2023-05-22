b26fba8a5d64b5d18f5ec78d0cb0e790bfda7020
senk0n <serhii@senk.one>
2023-05-22 09:43:01 +0200

    add kotlin-parcelize plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin)
     alias(libs.plugins.google.devtools.ksp)
+    id("kotlin-parcelize")
 }
 
 android {
