5c3752022cb3562a070fc4acf5db2857a2b706ab
senk0n <serhii@senk.one>
2023-05-24 23:09:06 +0200

    di provider setup for tab

---
+++ app/src/main/java/dev/senk0n/moerisuto/core/navigation/tabs/Tab.kt
@@ -0,0 +1,7 @@
+package dev.senk0n.moerisuto.core.navigation.tabs
+
+import dev.senk0n.moerisuto.core.navigation.ComponentConfig
+import dev.senk0n.moerisuto.core.navigation.ComponentView
+
+interface TabConfig : ComponentConfig
+interface TabComponentView : ComponentView
