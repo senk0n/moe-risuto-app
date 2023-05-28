15a2b69d67d7f65698ee74ab3827e719db66809c
senk0n <serhii@senk.one>
2023-05-29 01:29:23 +0200

    refactor di nav multibinding provider

---
+++ app/src/main/java/dev/senk0n/moerisuto/core/di/NavigationFactory.kt
@@ -0,0 +1,24 @@
+package dev.senk0n.moerisuto.core.di
+
+import com.arkivanov.decompose.ComponentContext
+import kotlin.reflect.KClass
+
+inline fun <reified Key : P, P : Any, R> provideComponent(
+    crossinline provider: ComponentContext.(Key) -> R
+): Pair<KClass<out P>, DepProvider<P, R>> = Key::class to DepProvider { config, context ->
+    val parameter = config as Key
+    context.provider(parameter)
+}
+
+interface NavigationFactory<Key : Any, out R : Any> {
+    val providerMap: Map<KClass<out Key>, DepProvider<Key, R>>
+    fun create(key: Key, context: ComponentContext): R? {
+        val provider = providerMap[key::class]
+        return provider?.provide(key, context)
+    }
+}
+
+fun interface DepProvider<in P, out R> {
+    fun provide(config: P, context: ComponentContext): R
+}
+
--- app/src/main/java/dev/senk0n/moerisuto/core/navigation/ComponentFactory.kt
+++ app/src/main/java/dev/senk0n/moerisuto/core/navigation/ComponentFactory.kt
@@ -1,11 +1,11 @@
 package dev.senk0n.moerisuto.core.navigation
 
 import dev.senk0n.moerisuto.core.di.DepProvider
-import dev.senk0n.moerisuto.core.di.MultibindingFactory
+import dev.senk0n.moerisuto.core.di.NavigationFactory
 import me.tatarka.inject.annotations.Inject
 import kotlin.reflect.KClass
 
 @Inject
 class ComponentFactory(
     override val providerMap: Map<KClass<out ComponentConfig>, DepProvider<ComponentConfig, ComponentView>>
-) : MultibindingFactory<ComponentConfig, ComponentView>
+) : NavigationFactory<ComponentConfig, ComponentView>
