6da39d27344e1b21e2a5f78f3b4b07ecef2462f2
senk0n <serhii@senk.one>
2023-05-30 09:32:14 +0200

    improve compose provider

--- app/src/main/java/dev/senk0n/moerisuto/core/navigation/compose/ComposeFactory.kt
+++ app/src/main/java/dev/senk0n/moerisuto/core/navigation/compose/ComposeFactory.kt
@@ -1,6 +1,7 @@
 package dev.senk0n.moerisuto.core.navigation.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import dev.senk0n.moerisuto.core.navigation.ComponentView
 import me.tatarka.inject.annotations.Inject
@@ -10,22 +11,19 @@ import kotlin.reflect.KClass
 class ComposeFactory(
     private val providerMap: Map<KClass<out ComponentView>, ComposeProvider>
 ) {
-    @Composable
-    fun Display(key: ComponentView, modifier: Modifier = Modifier, error: @Composable () -> Unit) {
-        val provider = providerMap[key::class]
-        provider?.Provide(key, modifier) ?: error()
+    fun create(key: ComponentView): ComposeProvider? {
+        return providerMap[key::class]
     }
 }
 
 fun interface ComposeProvider {
     @Composable
-    fun Provide(component: ComponentView, modifier: Modifier)
+    fun Display(component: ComponentView, modifier: Modifier)
 }
 
 inline fun <reified Component : ComponentView> provideCompose(
     crossinline provider: @Composable (Component, Modifier) -> Unit
 ): Pair<KClass<out ComponentView>, ComposeProvider> =
     Component::class to ComposeProvider { component, modifier ->
-        val parameter = component as Component
-        provider(parameter, modifier)
+        provider.invoke(component as Component, modifier)
     }
