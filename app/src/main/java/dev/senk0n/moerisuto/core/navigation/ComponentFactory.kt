8f4826b8a3530215758574b4a69a3d03494a736f
senk0n <serhii@senk.one>
2023-06-03 18:37:39 +0200

    refactor ComponentProvider
    - simplify by removing unnecessary inheritance

--- app/src/main/java/dev/senk0n/moerisuto/core/navigation/ComponentFactory.kt
+++ app/src/main/java/dev/senk0n/moerisuto/core/navigation/ComponentFactory.kt
@@ -1,11 +1,29 @@
 package dev.senk0n.moerisuto.core.navigation
 
-import dev.senk0n.moerisuto.core.navigation.di.DepProvider
-import dev.senk0n.moerisuto.core.navigation.di.NavigationFactory
+import com.arkivanov.decompose.ComponentContext
 import me.tatarka.inject.annotations.Inject
 import kotlin.reflect.KClass
 
 @Inject
 class ComponentFactory(
-    override val providerMap: Map<KClass<out ComponentConfig>, DepProvider<ComponentConfig, ComponentView>>
-) : NavigationFactory<ComponentConfig, ComponentView>
+    private val providerMap: Map<KClass<out ComponentConfig>, ComponentProvider>
+) {
+    fun create(key: ComponentConfig, context: ComponentContext): ComponentView {
+        val provider = providerMap[key::class]
+        return provider?.provide(key, context)
+            ?: providerMap[ComponentConfig::class]?.provide(key, context)
+            ?: error("no applicable ComponentProvider found")
+    }
+}
+
+fun interface ComponentProvider {
+    fun provide(config: ComponentConfig, context: ComponentContext): ComponentView
+}
+
+inline fun <reified Config : ComponentConfig> provideComponent(
+    crossinline provider: ComponentContext.(Config) -> ComponentView
+): Pair<KClass<out Config>, ComponentProvider> =
+    Config::class to ComponentProvider { config, context ->
+        val parameter = config as Config
+        context.provider(parameter)
+    }
