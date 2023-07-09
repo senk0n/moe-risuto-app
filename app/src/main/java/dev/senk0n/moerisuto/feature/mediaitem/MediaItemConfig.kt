f6f873cd5e441ebed65d7040b0d935dfdc7a88d1
senk0n <serhii@senk.one>
2023-07-09 23:50:57 +0200

    fix same type screens in stack
    temp solution, need to verify if it's the best possible solution

--- app/src/main/java/dev/senk0n/moerisuto/feature/mediaitem/MediaItemConfig.kt
+++ app/src/main/java/dev/senk0n/moerisuto/feature/mediaitem/MediaItemConfig.kt
@@ -2,9 +2,11 @@ package dev.senk0n.moerisuto.feature.mediaitem
 
 import com.arkivanov.essenty.parcelable.Parcelize
 import dev.senk0n.moerisuto.core.navigation.ComponentConfig
+import kotlin.random.Random
 
 @Parcelize
 data class MediaItemConfig(
     val entryId: String,
-    val serviceName: String
+    val serviceName: String,
+    val seed: Int = Random.nextInt(),
 ) : ComponentConfig
