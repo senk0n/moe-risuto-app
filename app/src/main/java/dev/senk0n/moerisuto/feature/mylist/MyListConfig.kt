11ea7f5e9c31c0e345a19b2cabf771994abcfab8
senk0n <serhii@senk.one>
2023-07-12 00:00:07 +0200

    change MyList parameters

--- app/src/main/java/dev/senk0n/moerisuto/feature/mylist/MyListConfig.kt
+++ app/src/main/java/dev/senk0n/moerisuto/feature/mylist/MyListConfig.kt
@@ -2,11 +2,10 @@ package dev.senk0n.moerisuto.feature.mylist
 
 import com.arkivanov.essenty.parcelable.Parcelize
 import dev.senk0n.moerisuto.core.navigation.ComponentConfig
-import kotlin.random.Random
 
 @Parcelize
 data class MyListConfig(
-    val format: String,
-    val progress: String,
-    val seed: Int = Random.nextInt(),
+    val format: String = "",
+    val progress: String = "",
+    val service: String = "",
 ) : ComponentConfig
