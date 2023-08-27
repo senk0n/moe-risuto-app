7fb615ff7f55d2e501bc33b085ec067c40d9ff46
senk0n <serhii@senk.one>
2023-08-27 23:45:53 +0200

    integrate ios
    initial setup to test compose ui

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,7 +1,23 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
-import dev.senk0n.moerisuto.App
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.statekeeper.StateKeeperDispatcher
+import dev.senk0n.moerisuto.root.RootComponentImpl
+import dev.senk0n.moerisuto.root.RootContent
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController {
-    return ComposeUIViewController { App() }
+    val lifecycle = LifecycleRegistry()
+    val stateKeeper = StateKeeperDispatcher()
+    val root = RootComponentImpl(
+        componentContext = DefaultComponentContext(
+            lifecycle = lifecycle,
+            stateKeeper = stateKeeper,
+        )
+    )
+    return ComposeUIViewController {
+        RootContent(component = root, modifier = Modifier.fillMaxSize())
+    }
 }
