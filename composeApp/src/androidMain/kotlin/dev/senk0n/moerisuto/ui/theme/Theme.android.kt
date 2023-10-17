24ddc2ccb6a69157b9811901fe7191e3deedc18f
senk0n <serhii@senk.one>
2023-10-18 01:28:53 +0200

    fix status bar color on android

--- composeApp/src/androidMain/kotlin/dev/senk0n/moerisuto/ui/theme/Theme.android.kt
+++ composeApp/src/androidMain/kotlin/dev/senk0n/moerisuto/ui/theme/Theme.android.kt
@@ -17,14 +17,14 @@ internal actual fun StatusBarColor(
     colorScheme: ColorScheme,
     darkTheme: Boolean
 ) {
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
+//    val view = LocalView.current
+//    if (!view.isInEditMode) {
+//        SideEffect {
+//            val window = (view.context as Activity).window
+//            window.statusBarColor = colorScheme.primary.toArgb()
+//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+//        }
+//    }
 }
 
 @Composable
